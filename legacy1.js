97caba0242cdc6d2b502c257c55df37f966e05d6
Infinite <111289762+InfiniteSmasher@users.noreply.github.com>
2023-10-14 15:13:39 +0400

    Update legacy1.js

--- legacy1.js
+++ legacy1.js
@@ -1,6 +1,6 @@
-let legacyBasicInverval = setInterval(() => {
+let legacyBasicInterval = setInterval(() => {
     if (typeof(BAWK) === "undefined" || !BAWK.sounds || !Object.keys(BAWK.sounds)[0] || !BAWK.sounds[Object.keys(BAWK.sounds)[0]].buffer) return;
-    clearInterval(legacyBasicInverval);
+    clearInterval(legacyBasicInterval);
     BAWK.load("https://cdn.jsdelivr.net/gh/InfiniteSmasher/Legacy-Mode@latest/sounds1.json");
     extern.catalog.findItemsByIds([3000, 3100, 3400, 3600, 3800, 4000, 4200]).forEach(item => {
         item.name = item.name.replace(" ", " Legacy ");
